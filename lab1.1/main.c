c069f4c21f3ca2c091dffd43ab140186ab8f9945
allexkap <aekpstn@gmail.com>
2023-03-12 12:38:17 +0300

    add error handling (lab1.1)

--- lab1.1/main.c
+++ lab1.1/main.c
@@ -5,10 +5,11 @@
 #include <unistd.h>
 
 
-void path_push(char *path, size_t *pos, char *name) {
-    size_t i = 0;
-    if (*pos) path[(*pos)++] = '/';
-    while (path[*pos] = name[i]) ++(*pos), ++i;
+int path_push(char *path, size_t *pos, char *name) {
+    if (*pos && *pos < PATH_MAX) path[(*pos)++] = '/';
+    for (size_t i = 0; *pos < PATH_MAX; ++(*pos), ++i)
+        if (!(path[*pos] = name[i])) return 0;
+    return 1;
 }
 
 void path_pop(char *path, size_t *pos) {
@@ -29,6 +30,10 @@ void nonRecursiveWalker() {
     struct dirent *entry;
 
     dirs[depth++] = opendir(".");
+    if (!dirs[depth-1]) {
+        fprintf(stderr, "Error opening this directory\n", path);
+        return;
+    }
 
     while (depth) {
         entry = readdir(dirs[depth-1]);
@@ -37,10 +42,17 @@ void nonRecursiveWalker() {
             if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
                 continue;
 
-            path_push(path, &pos, entry->d_name);
+            if (path_push(path, &pos, entry->d_name)) {
+                fprintf(stderr, "Error opening path %s: File name too long\n", path);
+                break;
+            }
 
             if (entry->d_type == DT_DIR) {
                 dirs[depth++] = opendir(path);
+                if (!dirs[depth-1]) {
+                    fprintf(stderr, "Error opening directory %s: Not enough file descriptors (%d)\n", path, depth);
+                    break;
+                }
                 continue;
             }
             if (entry->d_type == DT_REG)
@@ -51,6 +63,8 @@ void nonRecursiveWalker() {
         }
         path_pop(path, &pos);
     }
+
+    while (depth) closedir(dirs[--depth]);
 }
 
 

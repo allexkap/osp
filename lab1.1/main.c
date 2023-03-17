80dfa6eb16958a9ffec47f8abe3dd7babd174e6d
allexkap <aekpstn@gmail.com>
2023-03-17 13:10:35 +0300

    add getopt support (lab1.1)

--- lab1.1/main.c
+++ lab1.1/main.c
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <getopt.h>
 
 
 int path_push(char *path, size_t *pos, char *name) {
@@ -18,6 +19,7 @@ void path_pop(char *path, size_t *pos) {
 }
 
 
+char *pattern_wf = 0;
 void walk_func(char *path) {
     printf("%s\n", path);
 }
@@ -31,7 +33,7 @@ void nonRecursiveWalker() {
 
     dirs[depth++] = opendir(".");
     if (!dirs[depth-1]) {
-        fprintf(stderr, "Error opening this directory\n");
+        fprintf(stderr, "Error opening selected directory\n");
         return;
     }
 
@@ -70,8 +72,48 @@ void nonRecursiveWalker() {
 
 int main(int argc, char **argv) {
 
-    if (argc < 2) return 0;
-    if (chdir(argv[1]) == -1) return 1;
+    while (1) {
+        static const struct option long_options[] = {
+            {"help",    0, 0, 'h'},
+            {"version", 0, 0, 'v'},
+            {0, 0, 0, 0}
+        };
+        switch (getopt_long(argc, argv, "hv", long_options, 0)) {
+            case 'h':
+                fprintf(stdout, "help\n");
+                break;
+            case 'v':
+                fprintf(stdout, "version\n");
+                break;
+            case -1:
+                goto options_end;
+            default:
+                return 1;
+        }
+    }
+    options_end:
+
+    for (int i = 1, j = 0; i < argc; ++i) {
+        if (argv[i][0] == '-') continue;
+        switch (j++) {
+            case 0:
+                if (chdir(argv[i]) == -1) {
+                    fprintf(stderr, "Error opening directory %s\n", argv[i]);
+                    return 1;
+                }
+                break;
+            case 1:
+                pattern_wf = argv[i];
+                break;
+            default:
+                fprintf(stderr, "Too many arguments\n");
+                return 1;
+        }
+    }
+    if (!pattern_wf) {
+        fprintf(stderr, "Not enough arguments\n");
+        return 1;
+    }
 
     nonRecursiveWalker();
 

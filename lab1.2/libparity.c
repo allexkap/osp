a281e8531627494ea7b2b517bbc9f4fe19439374
allexkap <aekpstn@gmail.com>
2023-05-11 15:51:22 +0300

    unify info (lab1.2)

--- lab1.2/libparity.c
+++ lab1.2/libparity.c
@@ -10,7 +10,7 @@ int plugin_get_info(struct plugin_info* ppi) {
     static struct plugin_option sup_opts[] = {
         {{"parity", required_argument, (int*) &arg, 0}, "evens, odds or eq"},
     };
-    ppi->plugin_purpose = "Check which bytes are frequent";
+    ppi->plugin_purpose = "Check which bytes are the most frequent";
     ppi->plugin_author = "allexkap";
     ppi->sup_opts_len = 1;
     ppi->sup_opts = sup_opts;
